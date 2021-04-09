8f21082b9b4b8bd4fa23c0eee0700720b13118b9
nekoimi <nekoimime@gmail.com>
2021-04-09 09:27:25 +0800

    up

--- src/remove_duplicates_node.cc
+++ src/remove_duplicates_node.cc
@@ -21,6 +21,42 @@ struct ListNode *deleteDuplicates(struct ListNode *head) {
     return head;
 }
 
+int *reversePrint(struct ListNode *head, int *returnSize) {
+    int count = 0;
+    struct ListNode *slow = NULL;
+    struct ListNode *next = NULL;
+    while (head != NULL) {
+        next = head->next;
+        head->next = slow;
+        slow = head;
+        head = next;
+        count++;
+    }
+    int *res = (int *) malloc(count * sizeof(int));
+    for (int j = 0; j < count; ++j) {
+        res[j] = slow->val;
+        slow = slow->next;
+    }
+    *returnSize = count;
+    return res;
+}
+
+int *reversePrint2(struct ListNode *head, int *returnSize) {
+    int count = 0;
+    struct ListNode *p = head;
+    while (p != NULL) {
+        p = p->next;
+        count++;
+    }
+    *returnSize = count;
+    int *res = (int *) calloc(count, sizeof(int));
+    for (int j = count - 1; j >= 0; j--) {
+        res[j] = head->val;
+        head = head->next;
+    }
+    return res;
+}
+
 void runRemoveDuplicatesNode() {
     struct ListNode *head = newLinkedList(1);
     struct ListNode *node = head;
