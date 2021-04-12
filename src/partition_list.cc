f09f07165c64f24d5cfc0840e0919741be07f021
nekoimi <nekoimime@gmail.com>
2021-04-12 15:47:04 +0800

    partition list

--- src/partition_list.cc
+++ src/partition_list.cc
@@ -11,27 +11,40 @@ struct ListNode* partition(struct ListNode* head, int x){
     if (head == NULL || head->next == NULL) {
         return head;
     }
-    struct ListNode* p = head;
-    while (p != NULL) {
-        if (p->val >= x) {
-            printf("pv: %d \n", p->val);
-            p = head;
+    struct ListNode* node = (struct ListNode*) malloc(sizeof(struct ListNode));
+    node->val = -101;
+    node->next = head;
+    struct ListNode* p = node;
+    while (p != NULL && p->next != NULL) {
+        if (p->next->val >= x) {
+            struct ListNode* sp = p->next;
+            struct ListNode* spr = sp;
+            while (sp != NULL && sp->next != NULL) {
+                if (sp->next->val < x) {
+                    p->next = sp->next;
+                    p = p->next;
+                    sp->next = sp->next->next;
+                } else {
+                    sp = sp->next;
+                }
+            }
+            p->next = spr;
             break;
         }
         p = p->next;
     }
-    return head;
+    return node->next;
 }
 
 void runPartitionList() {
     struct ListNode* head = newLinkedList(1);
     struct ListNode* node = insert(head, 4);
     node = insert(node, 3);
+    node = insert(node, 0);
     node = insert(node, 2);
     node = insert(node, 5);
     node = insert(node, 2);
     printLinkedList(head);
-
     head = partition(head, 3);
     printLinkedList(head);
 }
