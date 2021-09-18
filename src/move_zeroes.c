e3d422b35aba6b2077ede3d8271396cea5405227
nekoimi <nekoimime@gmail.com>
2021-09-18 17:21:09 +0800

    update

--- src/move_zeroes.c
+++ src/move_zeroes.c
@@ -6,13 +6,27 @@
 #include "move_zeroes.h"
 
 void moveZeroes(int* nums, int numsSize) {
-
+    if (numsSize == 0) return;
+    int j = 0;
+    for (int i = 0; i < numsSize; ++i) {
+        if (nums[i] != 0) {
+            int tmp = nums[i];
+            nums[i] = nums[j];
+            nums[j++] = tmp;
+        }
+    }
 }
 
 void do_move_zero() {
-    int size = 7;
+    int size = 5;
     int *nums = malloc(sizeof(int) * size);
+    nums[0] = 0;
+    nums[1] = 1;
+    nums[2] = 5;
+    nums[3] = 0;
+    nums[4] = 9;
+    moveZeroes(nums, size);
     for (int i = 0; i < size; ++i) {
-        nums[i] = i + 1;
+        printf("%d, ", nums[i]);
     }
 }
